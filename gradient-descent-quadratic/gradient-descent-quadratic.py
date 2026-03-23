f7fa9f8dafe4e62fe9fc5e7454ffac0e81d75fb8
hieutmtechx <hieu.tm@techxcorp.com>
2026-03-23 11:37:21 +0700

    Update solution for gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -5,7 +5,7 @@ def func(a, b, c, x):
 def derivate(a, b, c, x):
     return (2 * a * x + b)
 
-def gradient_descent_quadratic(a, b, c, x0, lr, steps):
+def gradient_descent_quadratic(a, b, c, x0, lr0, steps):
     """
     Return final x after 'steps' iterations.
     """
@@ -13,11 +13,12 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     pass
 
     x = x0
-    while (steps > 0):
+    lr = lr0
+    for i in range(steps):
 
         nx = x - lr * derivate(a, b, c, x)
 
+        lr = lr0 / (1 + (i / 50))
         x = nx
-        steps -= 1
         
     return x
