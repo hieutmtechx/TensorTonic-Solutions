371e1dc222725d513b87d6ddfa0a9e9b4ff8957b
hieutmtechx <hieu.tm@techxcorp.com>
2026-03-23 17:24:03 +0700

    Add solution for adam-optimizer

---
+++ adam-optimizer/adam-optimizer.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def adam_step(params, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
+    """
+    One Adam optimizer update step.
+    Return (param_new, m_new, v_new).
+    """
+    params = np.asarray(params)
+    grad = np.asarray(grad)
+    m = np.asarray(m)
+    v = np.asarray(v)
+
+    # First moment (mean)
+    m = beta1 * m + (1 - beta1) * grad
+
+    # Second moment (variance)
+    v = beta2 * v + (1 - beta2) * (grad ** 2)
+
+    # Bias correction
+    m_hat = m / (1 - beta1 ** t)
+    v_hat = v / (1 - beta2 ** t)
+
+    # Parameter update
+    params = params - lr * m_hat / (np.sqrt(v_hat) + eps)
+
+    return params, m, v
