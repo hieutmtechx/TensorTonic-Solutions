5a58ba52a0df09255917ec1acd6bbd9c56903284
hieutmtechx <hieu.tm@techxcorp.com>
2026-03-31 12:08:16 +0700

    Update solution for weighted-moving-average

--- weighted-moving-average/weighted-moving-average.py
+++ weighted-moving-average/weighted-moving-average.py
@@ -13,9 +13,7 @@ def weighted_moving_average(values, weights):
     W = np.sum(np.asarray(weights))
     for i in range(n - k + 1):
         x = 0
-        print(i, "@@")
         for j in range(k):
-            print(i + k - j - 1)
             x += weights[j] * values[i + j]
         x /= W
         wma.append(x)
