8a96c5b78a08774d3c901d7e1a1d83417d6113a7
Minh Hieu Tran <hieu.tm@techxcorp.com>
2026-04-03 15:20:50 +0700

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -14,7 +14,6 @@ def positional_encoding(seq_len, d_model, base=10000.0):
         for d in range(d_model):
             i = d // 2
             frequency = 1 / np.power(base, (2 * i) / d_model)
-            print(frequency, d // d_model)
             encoding = np.cos(pos * frequency) if (d & 1) else np.sin(pos * frequency)
             current_position.append(encoding)
         position_encodings.append(current_position)
