25927a099b40fadd4210acced053fa5535f9c24c
Minh Hieu Tran <hieu.tm@techxcorp.com>
2026-04-09 10:31:31 +0700

    Add solution for leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    # Write code here
+    pass
+
+    x = np.asarray(x, dtype = float)
+    x = np.where(x < 0, x * alpha, x)
+    return x
